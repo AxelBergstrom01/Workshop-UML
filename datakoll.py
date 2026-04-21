5902dafbc4eb89245092cf17260aa9142fe38c71
Axel <gusberaxm@student.gu.se>
2026-04-21 10:19:15 +0200

    la till datakoll

---
+++ datakoll.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Ladda dataset
+df = pd.read_csv("osm_goteborg_pois.csv")
+
+print("=== INFO ===")
+print(df.info())
+
+print("\n=== BESKRIVNING ===")
+print(df.describe())
+
+print("\n=== MISSING VALUES ===")
+print(df.isnull().sum())
+
+print("\n=== HEAD ===")
+print(df.head())
